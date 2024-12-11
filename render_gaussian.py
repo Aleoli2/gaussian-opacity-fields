dba1483694b058c1b40f559e06fdc8770bdbfbef
Alejandro Olivas <alejandro.olivas@ua.es>
2024-12-11 11:56:59 +0100

    Update renderer

--- render_gaussian.py
+++ render_gaussian.py
@@ -63,40 +63,46 @@ class View(nn.Module):
         self.world_view_transform[:3,3] = t_inv
         self.world_view_transform = torch.tensor(np.float32(self.world_view_transform)).transpose(0, 1).cuda()
         
+class Renderer():
+    def __init__(self):
+        # Set up command line argument parser
+        parser = ArgumentParser(description="Testing script parameters")
+        
+        self.model = ModelParams(parser, sentinel=True)
+        self.pipeline = PipelineParams(parser)
+        parser.add_argument("--iteration", default=-1, type=int)
+        args = get_combined_args(parser)
 
-def render_view(camera_model, rotation, translation, gaussians, pipeline, background, kernel_size, idx):
-    view = View(rotation, translation, camera_model["fl_x"], camera_model["fl_y"], camera_model["w"], camera_model["h"])
-    rendering = render(view, gaussians, pipeline, background, kernel_size=kernel_size)["render"]
-    rendering = rendering[:3, :, :]
-    # image_path=os.path.join("/workspace/gaussian-opacity-fields/outputs", '{0:05d}'.format(idx) + ".png")
-    # torchvision.utils.save_image(rendering, image_path)
-    # os.chmod(image_path, 0o777)
+        # TDO load from config file
+        self.camera_model = { "fl_x": 914.7086181640625,
+        "fl_y": 912.5759887695312,
+        "w": 1280,
+        "h": 720}
+
+        dataset = self.model.extract(args)
+        self.gaussians = GaussianModel(dataset.sh_degree)
+        #Load model
+        scene = Scene(dataset, self.gaussians, load_iteration=args.iteration, shuffle=False, load_cameras=False)
+        bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
+        self.background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+        self.kernel_size = dataset.kernel_size
+
+
+    def render_view(self, rotation, translation):
+        view = View(rotation, translation, self.camera_model["fl_x"], self.camera_model["fl_y"], self.camera_model["w"], self.camera_model["h"])
+        rendering = render(view, self.gaussians, self.pipeline, self.background, kernel_size=self.kernel_size)["render"]
+        rendering = rendering[:3, :, :]
+        return rendering
+        # image_path=os.path.join("/workspace/gaussian-opacity-fields/outputs", '{0:05d}'.format(1) + ".png")
+        # torchvision.utils.save_image(rendering, image_path)
+        # os.chmod(image_path, 0o777)
 
 if __name__ == "__main__":
-    # Set up command line argument parser
-    parser = ArgumentParser(description="Testing script parameters")
-    model = ModelParams(parser, sentinel=True)
-    pipeline = PipelineParams(parser)
-    parser.add_argument("--iteration", default=-1, type=int)
-    args = get_combined_args(parser)
-    print("Rendering " + args.model_path)
-
-    camera_model = { "fl_x": 914.7086181640625,
-	"fl_y": 912.5759887695312,
-	"w": 1280,
-	"h": 720}
-
-    dataset = model.extract(args)
-    gaussians = GaussianModel(dataset.sh_degree)
-    #Load model
-    scene = Scene(dataset, gaussians, load_iteration=args.iteration, shuffle=False, load_cameras=False)
-    bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
-    background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
-    kernel_size = dataset.kernel_size
+    renderer = Renderer()
     R = np.array([[-0.9439490687833039, -0.012658517779477973, 0.32984832494763394], [-0.32913601383730196, -0.039867014542148096, -0.9434405681052662], [0.025092627172631866, -0.9991248085595327, 0.03346605716920529]])
     T = np.array([-12.574966256566968, 38.94981550209616, -0.06905938699614564])
     start=time.clock_gettime_ns(time.CLOCK_THREAD_CPUTIME_ID)
     for _ in range(30):
-        render_view(camera_model, R, T, gaussians,pipeline, background, kernel_size, 1)
+        renderer.render_view(R, T)
     end=time.clock_gettime_ns(time.CLOCK_THREAD_CPUTIME_ID)
     print("Mean rendering time (ms): {} FPS: {}".format((end-start)*10**-6/30,(1/((end-start)*10**-9)*30)))
